d8de414de55e3fdd52f217041186f16651021f8b
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-01-03 15:59:03 +0900

    feat: router 정의 및 component 연결

---
+++ src/pages/auth/UserSignup.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Signup page</h1>
+</template>
+
+<script setup>
+console.log("signup page");
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import UserLogin from "../pages/auth/UserLogin.vue";
+import UserSignup from "../pages/auth/UserSignup.vue";
 
 import App from "../App.vue";
-const routes = [{ path: "/", component: App }];
+const routes = [
+  { path: "/", component: App },
+  {
+    path: "/login",
+    component: UserLogin,
+  },
+  {
+    path: "/signup",
+    component: UserSignup,
+  },
+  {
+    path: "/chat",
+    component: null,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
